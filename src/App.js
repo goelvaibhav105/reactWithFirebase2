2b96cb851454f0fedb8350d447123b36cc505f1f
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-18 15:20:05 +0530

    Some css CHanges

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import User from "./components/User";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/" element={<User />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
